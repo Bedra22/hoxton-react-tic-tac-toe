b5698a56ab81c1cb85e5c35f397c0300603b4130
Bedra Kraja <bedra.kraja22@gmail.com>
2022-08-19 16:01:50 +0200

    Founded how the winner is just displaying it on page left

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,17 @@ import { Border } from './components/Border'
 function App() {
 
   const [border, setBorder] = useState(Array(9).fill(null))
-  const [firstPlayer, setFisrtPlayer] = useState(true);
+  const [firstPlayer, setFisrtPlayer] = useState(true)
+  const winnigLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
 
   const whosPlaying = (boxID) => {
     const borderPlaying = border.map((box, index) => {
@@ -19,10 +29,23 @@ function App() {
         return box
       }
     })
-    setFisrtPlayer(firstPlayer);
+    setFisrtPlayer(!firstPlayer);
 
     setBorder(borderPlaying)
 
+    andTheWinnerIs(borderPlaying)
+
+  }
+
+  const andTheWinnerIs = (box) => {
+    for (let i = 0; i < winnigLines.length; i++) {
+      const [box1, box2, box3] = winnigLines[i];
+      if (box[box1] && box[box1] === box[box2] && box[box1] === box[box3]) {
+        console.log(box[box1])
+        return box[box1];
+      }
+    }
+    return null;
   }
 
   return (
--- src/components/Border.tsx
+++ src/components/Border.tsx
@@ -4,7 +4,7 @@ export function Border({ border, onClick }) {
     return (
         <div className="border">
             {border.map((box, index) => (
-                <EachBox key={index} value={box} onClick={() => onClick(index)} />
+                <EachBox key={index} value={box} onClick={() => box === null && onClick(index)} />
             ))}
 
         </div>
